6221fc55a47f46f757eee6d549e91e12bb2f17e6
Freddy Tomada <freddytomada7@gmail.com>
2024-04-15 09:20:25 -0400

    add navbar

---
+++ src/app/not-found.jsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h2>Not Found</h2>
+        <p>Sorry, the page you are looking for does not exist.</p>
+        <Link href="/">
+            <span>Go back to the homepage</span>
+        </Link>
+    </div>
+  )
+}
